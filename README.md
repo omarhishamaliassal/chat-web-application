b447f86fdb13864c46715a2ec58c30d4cfc399a9
omarhishamaliassal <102229712+omarhishamaliassal@users.noreply.github.com>
2024-08-20 14:32:46 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# chat-web-application
-chat application end to end encryption using steganography
+# ChatSafe
+It is well known that Steganography and encryption together represent a great features to secure most of communications and provide data confidentiality and integrity. In this repo you'll see the effect of adding security layers to systems.
+Implementation of an encryption security layer to improve communication security. About Steganography we provided our tool Audstegnia to try within our Chat Application.
+
+
+## Authors
+
+- [@AkramHammam](https://www.github.com/akramhammam5)
+- [@OmarHisham](https://www.github.com/omarhishamxx)
+- [@OmarAssal](https://github.com/omarhishamaliassal)
+- [@MarwanMagdy](https://github.com/Marwan1241)
+
+
+## Features
+
+- Secure Communication 
+- Fast
+- Data integrity guranteed
+- Audstegnia feature to hide Text inside Audio file and Send it :)
+
+
+
+
+
+
+
+
+
+
--- urls.py
+++ urls.py
@@ -1,33 +1,22 @@
-#urls.py
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-from django.urls import path, include, reverse_lazy
-from django.conf import settings
-from django.conf.urls.static import static
+"""chatapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.home, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout_user, name='logout'),
-    path('chat/<int:pk>/', views.chat_view, name='chat'),
-    path('new_chat/<int:pk>', views.new_chat, name='new_chat'),
-     path('load_chat/<int:chat_id>/', views.load_chat, name='load_chat'),
-    path('chat/<int:chat_id>/create_voice_note/', views.create_voice_note, name='create_voice_note'),
-    path('call/', views.videocall, name='call'),
-    path('steg/', views.hide, name='steg'),
-    path('voice_notes/<int:voice_note_id>/', views.serve_voice_note, name='serve_voice_note'),
-    path('extract/', views.extract, name='extract'),
-    path('edit-profile/', views.edit_profile, name='edit_profile'),
-    path('record-and-send/', views.record_and_send, name='record_and_send'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='chat/password_reset_form.html'), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='chat/password_reset_done.html'), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='chat/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='chat/password_reset_complete.html'), name='password_reset_complete'),
-    path('payment/', views.payment, name='payment'),
-    path('receive/', views.receive_voice_notes, name='receive_voice_notes'),
-    path('playback/<int:voice_note_id>/', views.playback_voice_note, name='playback_voice_note'),
-    path('wav/', views.embedwav, name='WAV'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    path('admin/', admin.site.urls),
+    path('', include('chat.urls')),
+]
